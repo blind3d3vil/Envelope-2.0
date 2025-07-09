bf67825af55b8b5f3546df7a56a32dd1241bc559
alireza <82836134+blind3d3vil@users.noreply.github.com>
2025-07-09 10:09:10 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,18 +14,14 @@ Two years since this story began not just a relationship, but the unfolding of s
 You are not a part of my life you are its meaning. You’re in the way I think, the way I dream, the way I carry love now. Before you, I didn’t know this kind of depth existed.",
       "We’ve had late-night talks and early morning kisses, playful teasing and deep silence. We've had hard days. We’ve had passion and comfort, fire and softness and every version of us has only made me more certain:
 I want you. Every day. Every season. Every version of us still to come.
-
 I ache to be near you today. Not just to hold your body, but to feel your heartbeat sync with mine.
 To tell you face to face what I already feel in every part of me 
 That I’m yours.
 Completely.
 And I don’t want a future unless it has your voice in the morning, your name in my phone, your hair on my pillow, your laugh in our kitchen, and your love wrapped around every version of me I become.
-
 Even without being beside you today, I feel wrapped in us in everything we’ve been, and everything we’re becoming.
-
 So happy anniversary, my heart.
 My safest place. My wildest dream. My once in a lifetime.
-
 Forever doesn’t seem long enough when it’s with you.
 ",
     ],
